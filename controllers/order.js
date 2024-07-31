b060af66f6342aef6bd42fc193231ca3a354cf80
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2024-07-31 15:40:45 -0700

    Added

--- controllers/order.js
+++ controllers/order.js
@@ -26,7 +26,7 @@ const orderStatusSMS = require("twilio")(
 
 // Square setup
 const squareClient = new Client({
-	environment: Environment.Sandbox, //Sandbox, Production
+	environment: Environment.Production, //Sandbox, Production
 	accessToken: process.env.SQUARE_ACCESS_TOKEN_TEST,
 });
 
