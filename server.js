3bb7063739e7fb322f082c8c9db2884afdf7b604
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2024-08-03 20:54:56 -0700

    Added

--- server.js
+++ server.js
@@ -138,6 +138,11 @@ app.get("/", (req, res) => {
 	res.send("Hello From ecommerce API");
 });
 
+// Add a test route
+app.get("/test", (req, res) => {
+	res.send("Server is working");
+});
+
 // Create the io instance
 const io = socketIo(server, {
 	cors: {
@@ -154,7 +159,7 @@ app.set("io", io);
 // routes middlewares
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-// Schedule task to run every 15 minutes
+// Schedule task to run every 10 minutes
 cron.schedule("*/10 * * * *", async () => {
 	try {
 		console.log("Running scheduled task to fetch Printify orders");
@@ -163,7 +168,7 @@ cron.schedule("*/10 * * * *", async () => {
 		);
 		console.log("Scheduled Task for Printify");
 	} catch (error) {
-		console.error("Error during scheduled task:");
+		console.error("Error during scheduled task:", error);
 	}
 });
 
